caa33d254d9ea30b7554b85a5fed0f4ef78e1e03
patil22 <gp1723658@gmail.com>
2023-02-07 12:34:59 -0500

    ss

--- src/Component/About.js
+++ src/Component/About.js
@@ -1,32 +1,14 @@
-import React from "react";
+import React from 'react'
 
-const About = () => {
+ const About = () => {
   return (
-    <div className="about">
-      <h1 id="whoiam">
-        {" "}
-        <span id="who">Who </span> I am
-      </h1>
-      <hr id="hr"></hr>
-      <img id="aboutimg" src={require("./About1.jpg")} />
-      <p id="p1">
-        My name's Gaurav Patil and i enjoy creating things that live on the{" "}
-        <br /> internet.
-      </p>
-      <p id="p2">
-        I'm a Full-Stack Web Developer with the ability to learn and <br />
-        collaborate in rapidly changing environments and compositions.
-        <br />
-        Worked through 1000+ hours of Bootcamp structure learning <br />
-        JavaScript, Node.Js, React.Js, MongoDB, Express, HTML5, and CSS3. <br />{" "}
-        <br></br>I am passionate about solving technical problems, researching
-        and  <br/>
-        developing new sites and technologies. I undertake and challenge <br></br>
-        myself to always improve, by being proactive, diligent, responsible, <br></br> 
-        and committed to finding the perfect solution to any problem.
-      </p>
-    </div>
-  );
-};
+    <div className="about"> 
+     {/* <img src = {require('./about.png')}/> */}
+  {/* <p id='aaab'>My name's Amol Godse and i enjoy creating things that live on the <br/> internet</p> */}
+<p id='ppp'>My name's Gaurav </p>
+      
+  </div>
 
+  )
+}
 export default About;
